bb5ff7e88de82a95ca6a5f5fa9937d2f0a9de61b
unknown <pedro.damiao@ccc.ufcg.edu.br>
2023-08-20 13:13:36 -0300

    projeto task-generator criado

---
+++ task-generator/src/main/java/com/ufcg/taskgenerator/TaskGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.ufcg.taskgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskGeneratorApplication.class, args);
+	}
+
+}
---
+++ task-generator/src/test/java/com/ufcg/taskgenerator/TaskGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufcg.taskgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
