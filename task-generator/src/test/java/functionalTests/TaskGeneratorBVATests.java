85be395c95e37f33b6ea5a845f7eba63138b215e
Pedro Damião <pedro.damiao@ccc.ufcg.edu.br>
2023-09-11 16:04:16 -0300

    testes funcionais criados

---
+++ task-generator/src/test/java/functionalTests/TaskGeneratorBVATests.java
@@ -0,0 +1,192 @@
+package functionalTests;
+
+import com.ufcg.taskgenerator.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.format.DateTimeFormatter;
+import java.time.format.ResolverStyle;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TaskGeneratorBVATests {
+
+    @Autowired
+    private TaskService taskServ;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @BeforeEach
+    void newRepoHashmap(){this.taskRepository.deleteAll();}
+
+    DateTimeFormatter formatter = DateTimeFormatter
+            .ofPattern("dd-MM-uuuu")
+            .withResolverStyle(ResolverStyle.STRICT);
+
+    @Test
+    void Should_CreateTask_When_ValidDateForCommonYear_LowestBoundary() throws Exception {
+        // Arrange
+        TaskDTO taskDTO = new TaskDTO(
+                "test",
+                "test discription",
+                "01-02-2025",
+                PRIORITY.LOW);
+        // Test
+        Task newTask = taskServ.createTask(taskDTO);
+        // Assert
+        assertEquals(taskDTO.getDate(), newTask.getExpirationDate().parse(dateAsString, formatter));
+    }
+
+    @Test
+    void Should_CreateTask_When_ValidDateForCommonYear() throws Exception {
+        // Arrange
+        TaskDTO taskDTO = new TaskDTO(
+                "test",
+                "test discription",
+                "15-02-2025",
+                PRIORITY.LOW);
+        // Test
+        Task newTask = taskServ.createTask(taskDTO);
+        // Assert
+        assertEquals(taskDTO.getDate(), newTask.getExpirationDate().parse(dateAsString, formatter));
+    }
+
+    @Test
+    void Should_CreateTask_When_ValidDateForCommonYear_HighestBoundary_30DayMonth() throws Exception {
+        // Arrange
+        TaskDTO taskDTO = new TaskDTO(
+                "test",
+                "test discription",
+                "30-04-2025",
+                PRIORITY.LOW);
+        // Test
+        Task newTask = taskServ.createTask(taskDTO);
+        // Assert
+        assertEquals(taskDTO.getDate(), newTask.getExpirationDate().parse(dateAsString, formatter));
+    }
+
+    @Test
+    void Should_ThrowExcepton_When_InvalidDateForCommonYear_HighestBoundary_30DayMonth() throws Exception {
+        // Arrange
+        TaskDTO taskDTO = new TaskDTO(
+                "test",
+                "test discription",
+                "31-04-2025",
+                PRIORITY.LOW);
+        // Assert
+        assertThrows(Exception.class, () -> taskServ.createTask(taskDTO));
+    }
+
+    @Test
+    void Should_CreateTask_When_ValidDateForCommonYear_InBoundary_31DayMonth() throws Exception {
+        // Arrange
+        TaskDTO taskDTO = new TaskDTO(
+                "test",
+                "test discription",
+                "30-01-2025",
+                PRIORITY.LOW);
+        // Test
+        Task newTask = taskServ.createTask(taskDTO);
+        // Assert
+        assertEquals(taskDTO.getDate(), newTask.getExpirationDate().parse(dateAsString, formatter));
+    }
+
+    @Test
+    void Should_CreateTask_When_ValidDateForCommonYear_HighestBoundary_31DayMonth() throws Exception {
+        // Arrange
+        TaskDTO taskDTO = new TaskDTO(
+                "test",
+                "test discription",
+                "31-01-2025",
+                PRIORITY.LOW);
+        // Test
+        Task newTask = taskServ.createTask(taskDTO);
+        // Assert
+        assertEquals(taskDTO.getDate(), newTask.getExpirationDate().parse(dateAsString, formatter));
+    }
+
+    @Test
+    void Should_CreateTask_When_ValidDateForCommonYear_HighestBoundary_February() throws Exception {
+        // Arrange
+        TaskDTO taskDTO = new TaskDTO(
+                "test",
+                "test discription",
+                "28-02-2025",
+                PRIORITY.LOW);
+        // Test
+        Task newTask = taskServ.createTask(taskDTO);
+        // Assert
+        assertEquals(taskDTO.getDate(), newTask.getExpirationDate().parse(dateAsString, formatter));
+    }
+
+    @Test
+    void Should_NotCreateTask_When_InvalidDateForCommonYear_February29() throws Exception {
+        // Arrange
+        TaskDTO taskDTO = new TaskDTO(
+                "test",
+                "test discription",
+                "29-02-2025",
+                PRIORITY.LOW);
+        // Assert
+        assertThrows(Exception.class, () -> taskServ.createTask(taskDTO));
+    }
+
+    @Test
+    void Should_NotCreateTask_When_InvalidDateForCommonYear_February30() throws Exception {
+        // Arrange
+        TaskDTO taskDTO = new TaskDTO(
+                "test",
+                "test discription",
+                "30-02-2025",
+                PRIORITY.LOW);
+        // Assert
+        assertThrows(Exception.class, () -> taskServ.createTask(taskDTO));
+    }
+
+    @Test
+    void Should_CreateTask_When_ValidDateForLeapYear_February28() throws Exception {
+        // Arrange
+        TaskDTO taskDTO = new TaskDTO(
+                "test",
+                "test discription",
+                "28-02-2024",
+                PRIORITY.LOW);
+        // Test
+        Task newTask = taskServ.createTask(taskDTO);
+        // Assert
+        assertEquals(taskDTO.getDate(), newTask.getExpirationDate().parse(dateAsString, formatter));
+    }
+
+    @Test
+    void Should_CreateTask_When_ValidDateForLeapYear_HighestBoundary_February() throws Exception {
+        // Arrange
+        TaskDTO taskDTO = new TaskDTO(
+                "test",
+                "test discription",
+                "29-02-2024",
+                PRIORITY.LOW);
+        // Test
+        Task newTask = taskServ.createTask(taskDTO);
+        // Assert
+        assertEquals(taskDTO.getDate(), newTask.getExpirationDate().parse(dateAsString, formatter));
+    }
+
+    @Test
+    void Should_NotCreateTask_When_invalidDateForLeapYear_February30() throws Exception {
+        // Arrange
+        TaskDTO taskDTO = new TaskDTO(
+                "test",
+                "test discription",
+                "28-02-2025",
+                PRIORITY.LOW);
+        // Assert
+        assertThrows(Exception.class, () -> taskServ.createTask(taskDTO));
+    }
+}
